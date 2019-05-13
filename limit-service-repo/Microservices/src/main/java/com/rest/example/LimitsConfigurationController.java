a5579db6b2380c301db04236809f1f4923de40b4
pritisharma54 <48624579+pritisharma54@users.noreply.github.com>
2019-05-13 10:03:58 +0100

    Add files via upload
    limit-service

---
+++ limit-service-repo/Microservices/src/main/java/com/rest/example/LimitsConfigurationController.java
@@ -0,0 +1,18 @@
+package com.rest.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	@GetMapping("/limits")
+	public LimitsConfiguration retLimitsFromConfiguration() {
+		//return new LimitsConfiguration(1000,1);
+		return new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+
+}
---
+++ limit-service-repo/Microservices/src/main/java/com/rest/example/MicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.rest.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//this is limit service
+@SpringBootApplication
+public class MicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesApplication.class, args);
+	}
+
+}
